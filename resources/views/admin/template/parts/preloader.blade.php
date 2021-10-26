96b7f96a75b97e2311d1915abbe6d5f96cbe9166
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2021-10-26 12:35:18 +0700

    fix preloader admin

--- resources/views/admin/template/parts/preloader.blade.php
+++ resources/views/admin/template/parts/preloader.blade.php
@@ -1,3 +1,5 @@
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('assets/images/picture.svg') }}" alt="preloader" height="120" width="120">
+    <img class="animation__shake" src="{{ asset('assets/images/picture.svg') }}"
+        style="animation-iteration-count: infinite !important;" alt="preloader" height="120" width="120">
+        <h1 class="animation__shake" style="animation-iteration-count: infinite !important;">Loading...</h1>
 </div>
